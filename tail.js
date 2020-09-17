84f79e5aceac42af112a0ea6cf4793351645b7a2
Aleksa Gavrilovic <lks_g@live.ca>
2020-09-17 01:43:44 -0400

    updates

--- tail.js
+++ tail.js
@@ -1,14 +1,14 @@
 const assertEqual = function(actual, expected) {
   return (actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`));
-  };
+};
 
-  const tail = function(array) {
-    let newArray = array.slice(1);
-    return newArray;
-  };
+const tail = function(array) {
+  let newArray = array.slice(1);
+  return newArray;
+};
   
-  const result = tail([1,2,3])
+const result = tail([1,2,3]);
 
-  console.log(result)
+console.log(result);
 
-  assertEqual(result[0],2)
+assertEqual(result[0],2);
