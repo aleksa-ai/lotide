f0d2c82a2e6148ed789e1c71dca255349cc24e04
Aleksa Gavrilovic <lks_g@live.ca>
2020-11-23 16:31:42 -0500

    created assertEqual function in 2 steps per activity instruction

--- assertEqual.js
+++ assertEqual.js
@@ -1,7 +1,21 @@
+//Step 1:
+/*const assertEqual = function(actual, expected) {
+  return actual === expected;
+};
+
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.log(assertEqual(1, 1));
+*/
+
+//Step 2:
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
