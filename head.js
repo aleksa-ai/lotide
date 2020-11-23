0642470ff9f375aa2a4ef123b020f69d688f9106
Aleksa Gavrilovic <lks_g@live.ca>
2020-11-23 17:05:55 -0500

    created the head function

--- head.js
+++ head.js
@@ -1,6 +1,10 @@
 const assertEqual = function(actual, expected) {
-  return (actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`));
-  };
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
 
 const head = function(array) {
   return array[0];
@@ -8,5 +12,7 @@ const head = function(array) {
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([7]), 5);
-assertEqual(head([]), 5);
+
+//Extra testing
+//assertEqual(head([7]), 5);
+//assertEqual(head([]), 5);
