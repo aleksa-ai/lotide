5b1a18ae230413ec8237380e238c0a91068ce4a5
Aleksa Gavrilovic <lks_g@live.ca>
2020-11-25 14:49:12 -0500

    updated middle & eqArrays function & linted

--- middle.js
+++ middle.js
@@ -1,49 +1,50 @@
 const eqArrays = function(arrayOne, arrayTwo) {
-if(arrayOne.length !== arrayTwo.length){
-  return false
-}
-  let result = true;
-   for (let i = 0; i < arrayOne.length ; i++) {
-     if (arrayOne[i] !== arrayTwo[i]){
-      result = false;
-     }
-    }
-    return result;
+  // /* Old way: */
+  // let result = true;
+  //  for (let i = 0; i < arrayOne.length ; i++) {
+  //    if (arrayOne[i] !== arrayTwo[i]){
+  //     result = false;
+  //    }
+  //   }
+  //   return result;
+  /*Simpler way*/
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  } else {
+    return true;
   }
+};
 
-  //passing two arrays, the actual one and what we expect it to be
-  //returns a console.log
-  const assertArraysEqual = function(actual, expected){
-    if (eqArrays(actual, expected) === true) {
-      console.log("✅✅✅ Assertion Passed")
-   }else{
-     console.log("❌❌❌ Assertion Failed")
-   }
+//passing two arrays, the actual one and what we expect it to be
+//returns a console.log
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected) === true) {
+    console.log("✅✅✅ Assertion Passed");
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed");
   }
+};
 
 
-const middle = function(array){
+const middle = function(array) {
   if (array.length < 3) {
-    return []
-  }
-  else if (array.length % 2 === 0){
-    const middleNumberEven = Math.floor((array.length-1)/2)
+    return [];
+  } else if (array.length % 2 === 0) {
+    const middleNumberEven = Math.floor((array.length - 1) / 2);
     //console.log(middleNumberEven)
-    return [array[middleNumberEven], array[middleNumberEven+1]]
+    return [array[middleNumberEven], array[middleNumberEven + 1]];
+  } else {
+    //const middleNumberOdd = Math.floor(array.length/2)
+    return [array[Math.floor(array.length / 2)]];
   }
-  else {
-    const middleNumberOdd = Math.floor(array.length/2)
-    //console.log([middleNumberOdd])
-    return [array[middleNumberOdd]]
-  }
-}
+};
 
-  assertArraysEqual(middle([1]),[])
-  assertArraysEqual(middle([1,2]),[])
-  assertArraysEqual(middle([1,2,3]),[2])
-  assertArraysEqual(middle([1,2,3,4,5]),[3])
-  //assertArraysEqual(middle([6,7,8,9,10]),[8])
-  assertArraysEqual(middle([1,2,3,4]),[2,3])
-  assertArraysEqual(middle([1,2,3,4,5,6]),[3,4])
-  assertArraysEqual(middle([11,12,13,14,15,16]),[13,14])
+assertArraysEqual(middle([1]),[]);
+assertArraysEqual(middle([1,2]),[]);
+assertArraysEqual(middle([1,2,3]),[2]);
+assertArraysEqual(middle([1,2,3,4,5]),[3]);
+assertArraysEqual(middle([6,7,8,9,10]),[8]);
+assertArraysEqual(middle([1,2,3,4]),[2,3]);
+assertArraysEqual(middle([1,2,3,4,5,6]),[3,4]);
+assertArraysEqual(middle([11,12,13,14,15,16]),[13,14]);
   
