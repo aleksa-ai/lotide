0b31fb390a291556f3a2c350987537ca24eee892
Aleksa Gavrilovic <lks_g@live.ca>
2020-11-26 01:55:58 -0500

    implemented findKeyByValue function l-ed

---
+++ findKeyByValue.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  return (actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`));
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+const findKeyByValue = function(object, value) {
+  const resultKey = [];
+  for (const key in object) {
+    //console.log(key);
+    if (object[key] === value) {
+      resultKey.push(key);
+    }
+  }
+  //console.log(results)
+  return resultKey[0];
+};
+
+// console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"))
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
