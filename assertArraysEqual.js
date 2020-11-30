165b1c3f225e0cf575e3f427a2253ba86def6fe5
Aleksa Gavrilovic <lks_g@live.ca>
2020-11-30 18:30:02 -0500

    refactored: implemented test, lted

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,20 +1,11 @@
-const eqArrays = function(arrayOne, arrayTwo) {
-  let result = true;
-   for (let i = 0; i < arrayOne.length ; i++) {
-     if (arrayOne[i] !== arrayTwo[i]){
-      result = false;
-     }
-    }
-    return result;
-  }
+const eqArrays = require('./eqArrays.js');
 
-  //this assertion function will make use of eqArrays function for array comparison.
-  const assertArraysEqual = function(actual, expected){
-    if (eqArrays(actual, expected) === true) {
-      console.log("✅✅✅ Assertion Passed")
-   }else{
-     console.log("🛑🛑🛑 Assertion Failed")
-   }
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected) === true) {
+    console.log("✅✅✅ Assertion Passed");
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed");
   }
+};
 
-  assertArraysEqual([1, 2, 3], [1, 2, 3]);
+module.exports = assertArraysEqual;
