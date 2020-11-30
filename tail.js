ca38bbb8bd1c8b73d919cf85079fdc500e437be5
Aleksa Gavrilovic <lks_g@live.ca>
2020-11-30 17:07:25 -0500

    refactored & implemented test

--- tail.js
+++ tail.js
@@ -1,24 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(array){
   let newArray = array.slice(1);
   return newArray;
 };
 
-let array = [1,2,3,4,5];
-
-const result = tail(array)
-
-assertEqual(result[0], array[1]);
-
-
-/*
-//Hard-coded version:  
-assertEqual(result[0],2);
-*/
+module.exports = tail;
