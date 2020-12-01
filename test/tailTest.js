58704f5e86b4ea091d27194fe1397e300e063252
Aleksa Gavrilovic <lks_g@live.ca>
2020-11-30 21:35:45 -0500

    Refactored tail test to use Mocha & Chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,4 +1,8 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-assertEqual(tail([1,2,3,4,5])[0], 2);
+describe("#tail", () => {
+  it("returns 2 for [1,2,3,4,5]", () => {
+    assert.strictEqual(tail([1,2,3,4,5])[0], 2);
+  })
+});
